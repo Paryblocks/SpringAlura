585cfcd13de53aedf6472899ed9257937903a366
Paryblocks <matheus4steiner9silva@gmail.com>
2026-06-04 19:55:03 -0300

    Pesquisa de serie, temporadas e episodios (Organização do código e lambda)

--- src/main/java/br/com/screenmatch/service/IConverteDados.java
+++ src/main/java/br/com/screenmatch/service/IConverteDados.java
@@ -1,6 +1,6 @@
 package br.com.screenmatch.service;
 
 public interface IConverteDados {
-    // <T> T ---> Significa objeto generico, usado para não especificar
+    // <T> T ---> Significa objeto generico, usado para trabalhar com parametros desconhecidos
     <T> T obterDados(String json, Class<T> classe);
 }
